b4eb832b788b1d33396247c9975b692374db1400
noion0511 <noion0511@gmail.com>
2022-06-07 22:06:19 +0900

    UserProfile

---
+++ src/main/java/com/example/novel/controller/UserProfileController.java
@@ -0,0 +1,27 @@
+package com.example.novel.controller;
+
+import com.example.novel.model.UserProfile;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import javax.annotation.PostConstruct;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class UserProfileController {
+     private Map<String, UserProfile> userMap;
+
+     @PostConstruct
+     public void init() {
+         userMap = new HashMap<String, UserProfile>();
+         userMap.put("1", new UserProfile("1", "홍길동", "111-1111", "서울시 강남구 대치1동"));
+         userMap.put("2", new UserProfile("2", "홍길자", "111-1112", "서울시 강남구 대치2동"));
+         userMap.put("3", new UserProfile("3", "홍길순", "111-1113", "서울시 강남구 대치3동"));
+     }
+
+     @GetMapping("/user/{id}")
+     public UserProfile getUserProfile(@PathVariable("id") String id) {
+         return userMap.get(id);
+     }
+}
